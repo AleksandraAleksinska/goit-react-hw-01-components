a0399af37d23b634c3791b66c0b0a7c59a704ef7
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-05-15 21:03:49 +0200

    statistics update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends';
 import transactions from '../data/transactions';
-import Statistics from "./Statistics";
+import Statistics from '../components/Statistics/Statistics';
 import FriendList from "./FriendList";
 import TransactionHistory from "./TransactionHistory";
 
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import css from './Statistics.module.css'
+
+const Statistics = ({title, stats}) => {
+  return (
+  <section class={css.statistics}>
+    <h2 class={css.title}>{title}</h2>
+
+    <ul class={css.statList}>
+
+    {stats.map(s=> (
+        <li key={s.id}class={css.item}>
+        <span class={css.label}>{s.label} </span>
+        <span class={css.percentage}>{s.percentage}%</span>
+      </li>
+    ))}
+
+    </ul>
+  </section>
+  )
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array
+}
+
+export default Statistics
